5680ef1be80d845ff9e4a445a41a232f08472d4a
vijeth11 <vijeth11@yahoo.co.in>
2021-01-17 14:21:06 -0500

    added Docker files and compose

--- deploy/update.sh
+++ deploy/update.sh
@@ -7,6 +7,11 @@ PROJECT_BASE_PATH='/usr/local/apps/simplesocial'
 git pull
 $PROJECT_BASE_PATH/env/bin/python manage.py migrate
 $PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
+
+# Configure supervisor
+cp $PROJECT_BASE_PATH/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/simplesocial.conf
+supervisorctl reread
+supervisorctl update
 supervisorctl restart simplesocial
 
 echo "DONE! :)"
